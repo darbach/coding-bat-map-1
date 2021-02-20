8872bf9f2d0908c119f10315463713b4f22829e1
darbach <danielarbach@gmail.com>
2021-02-19 17:03:24 -0700

    Added mapShare solution.

--- src/main/java/edu/cnm/deepdive/Map1.java
+++ src/main/java/edu/cnm/deepdive/Map1.java
@@ -26,11 +26,30 @@ public class Map1 {
    * @return Modified map
    */
   public Map<String, String> mapBully(Map<String, String> map) {
-    String aValue = map.get("a");
-    if (aValue != null) {
-      map.put("b", aValue);
+    if (map.containsKey("a")) {
+      map.put("b", map.get("a"));
       map.put("a", "");
     }
+    return map;
+  }
+
+  /**
+   * Modify and return the given map as follows: if the key "a" has a value, set the key "b"
+   * to have that same value. In all cases remove the key "c", leaving the rest of the map
+   * unchanged.
+   *
+   * mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
+   * mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
+   * mapShare({"a": "aaa", "c": "meh", "d": "hi"}) → {"a": "aaa", "b": "aaa", "d": "hi"}
+   *
+   * @param map A Map<String, String>
+   * @return Modified map
+   */
+  public Map<String, String> mapShare(Map<String, String> map) {
+    if (map.containsKey("a")) {
+      map.put("b", map.get("a"));
+    }
+    map.remove("c");
     return map;
   }
 }
--- src/test/java/edu/cnm/deepdive/Map1Test.java
+++ src/test/java/edu/cnm/deepdive/Map1Test.java
@@ -2,8 +2,6 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.AbstractMap;
-import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -18,29 +16,59 @@ class Map1Test {
   @Test
   void mapBully() {
     List<Map<String, String>> mapBullyParams = new ArrayList<>(Arrays.asList(
-        new HashMap<String, String>(Map.of("a", "candy", "b", "dirt")),
-        new HashMap<String, String>(Map.of("a", "candy")),
-        new HashMap<String, String>(Map.of("a", "candy", "b", "carrot", "c", "meh")),
-        new HashMap<String, String>(Map.of("b", "carrot")),
-        new HashMap<String, String>(Map.of("c", "meh")),
-        new HashMap<String, String>(Map.of("a", "sparkle", "c", "meh"))
+        new HashMap<>(Map.of("a", "candy", "b", "dirt")),
+        new HashMap<>(Map.of("a", "candy")),
+        new HashMap<>(Map.of("a", "candy", "b", "carrot", "c", "meh")),
+        new HashMap<>(Map.of("b", "carrot")),
+        new HashMap<>(Map.of("c", "meh")),
+        new HashMap<>(Map.of("a", "sparkle", "c", "meh"))
     ));
     List<Map<String, String>> mapBullyExpected = new ArrayList<>(Arrays.asList(
-        new HashMap<String, String>(Map.of("a", "", "b", "candy")),
-        new HashMap<String, String>(Map.of("a", "", "b", "candy")),
-        new HashMap<String, String>(Map.of("a", "", "b", "candy", "c", "meh")),
-        new HashMap<String, String>(Map.of("b", "carrot")),
-        new HashMap<String, String>(Map.of("c", "meh")),
-        new HashMap<String, String>(Map.of("a", "", "b", "sparkle", "c", "meh"))
+        new HashMap<>(Map.of("a", "", "b", "candy")),
+        new HashMap<>(Map.of("a", "", "b", "candy")),
+        new HashMap<>(Map.of("a", "", "b", "candy", "c", "meh")),
+        new HashMap<>(Map.of("b", "carrot")),
+        new HashMap<>(Map.of("c", "meh")),
+        new HashMap<>(Map.of("a", "", "b", "sparkle", "c", "meh"))
     ));
-    System.out.println("Running mapBully() test...");
+    System.out.println("\nRUNNING mapBully() TEST...");
     for (int i = 0; i < mapBullyParams.size(); i++) {
       String param = mapBullyParams.get(i).toString();
       Map<String, String> expected = mapBullyExpected.get(i);
       Map<String, String> actual = map1.mapBully(mapBullyParams.get(i));
-      System.out.printf("Param: %s; Expected: %s; Actual %s%n",
+      System.out.printf("PARAM, %s; EXPECTED, %s; ACTUAL %s%n",
           param, expected.toString(), actual.toString());
-      assertTrue(expected.equals(actual));
+      assertEquals(actual, expected);
+    }
+  }
+
+  @Test
+  void mapShare() {
+    List<Map<String, String>> mapShareParams = new ArrayList<>(Arrays.asList(
+        new HashMap<>(Map.of("a", "aaa", "b", "bbb", "c", "ccc")),
+        new HashMap<>(Map.of("b", "xyz", "c", "ccc")),
+        new HashMap<>(Map.of("a", "aaa", "c", "meh", "d", "hi")),
+        new HashMap<>(Map.of("a", "xyz", "b", "1234", "c", "yo", "z",
+            "zzz")),
+        new HashMap<>(Map.of("a", "xyz", "b", "1234", "c", "yo", "d",
+            "ddd", "e", "everything"))
+    ));
+    List<Map<String, String>> mapShareExpected = new ArrayList<>(Arrays.asList(
+        new HashMap<>(Map.of("a", "aaa", "b", "aaa")),
+        new HashMap<>(Map.of("b", "xyz")),
+        new HashMap<>(Map.of("a", "aaa", "b", "aaa", "d", "hi")),
+        new HashMap<>(Map.of("a", "xyz", "b", "xyz", "z", "zzz")),
+        new HashMap<>(Map.of("a", "xyz", "b", "xyz", "d", "ddd", "e",
+            "everything"))
+    ));
+    System.out.println("\nRUNNING mapShare() TEST...");
+    for (int i = 0; i < mapShareParams.size(); i++) {
+      String param = mapShareParams.get(i).toString();
+      Map<String, String> expected = mapShareExpected.get(i);
+      Map<String, String> actual = map1.mapShare(mapShareParams.get(i));
+      System.out.printf("PARAM, %s; EXPECTED, %s; ACTUAL %s%n",
+          param, expected.toString(), actual.toString());
+      assertEquals(actual, expected);
     }
   }
 }
